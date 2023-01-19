06ca3fe73ee005550d78de8208906b7d2b81fb10
GuminskiAdrian <guminskiadrian34@gmail.com>
2023-01-19 17:12:12 +0100

    get and post works, working on delete request

--- back/app.js
+++ back/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS, PATCH');
     res.setHeader('Access-Control-Allow-headers', 'Content-Type');
     next();
 });
--- back/routes_test/posts.js
+++ back/routes_test/posts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Model } = require("mongoose");
 const router = express.Router();
 const Post = require("../models/Posts");
 
@@ -25,6 +26,16 @@ router.post("/", async (req, res) => {
         console.log(err)
         console.log(req.title);
         console.log(req.description);
+    }
+});
+
+router.delete("/", async (req, res) => {
+    try {
+        const id = req.params.id;
+        Post.findByIdAndDelete(id);
+        console.log('hej');
+    } catch (err) {
+        res.json({ message: err });
     }
 });
 
